9e41fc638606f4c16854daaffbca70af80f789fa
Alexey Storozhev <dark.storoj@gmail.com>
2014-02-26 02:44:28 +0400

    added sample code to get outlets

--- Runtime Demo/ELAppDelegate.m
+++ Runtime Demo/ELAppDelegate.m
@@ -7,8 +7,19 @@
 //
 
 #import "ELAppDelegate.h"
+#import "utilities.h"
 
 @implementation ELAppDelegate
+
++ (void)initialize
+{
+    Class connectionClass = NSClassFromString(@"UIRuntimeConnection");
+    Method method = class_getInstanceMethod(connectionClass, NSSelectorFromString(@"initWithCoder:"));
+
+    method_swizzle1_id(method, NULL, ^void(id selfAfter, SEL pSelector, id argument) {
+        NSLog(@"%@ %@ %@", [selfAfter valueForKey:@"label"], [[selfAfter valueForKey:@"source"] class], [[selfAfter valueForKey:@"destination"] class]);
+    });
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
