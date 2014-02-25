86b95ea3a1dcd9d9f66939ce2f40d58ef3514c68
Alexey Storozhev <dark.storoj@gmail.com>
2014-02-25 20:23:07 +0400

    added property setter selector construction functions

--- Runtime Demo/utilities.m
+++ Runtime Demo/utilities.m
@@ -5,6 +5,30 @@
 #import "utilities.h"
 #import <objc/runtime.h>
 
+static NSString *setterNameForPropertyWithName(NSString *propertyName)
+{
+    NSString *firstLetter = [[propertyName substringToIndex:1] uppercaseString];
+    NSString *remainingSetterName = [propertyName substringFromIndex:1];
+    return [NSString stringWithFormat:@"set%@%@:", firstLetter, remainingSetterName];
+}
+
+static NSString *property_getSetterName(objc_property_t property)
+{
+    char *setterAttributeValue = property_copyAttributeValue(property, "S");
+
+    if (NULL != setterAttributeValue) {
+        NSString *setterName = [NSString stringWithUTF8String:setterAttributeValue];
+        
+        // do not forget!
+        free(setterAttributeValue);
+
+        return setterName;
+    }
+
+    const char *propertyName = property_getName(property);
+    return setterNameForPropertyWithName([NSString stringWithUTF8String:propertyName]);
+}
+
 void observeClassPropertyChanges(Class class)
 {
     unsigned int propertyCount = 0;
