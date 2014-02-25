e3a4abe19b35a0002a9333fc47a1698958015f6d
Alexey Storozhev <dark.storoj@gmail.com>
2014-02-25 19:11:54 +0400

    getting property list

--- Runtime Demo/utilities.m
+++ Runtime Demo/utilities.m
@@ -3,8 +3,15 @@
 // Copyright (c) 2014 e-Legion. All rights reserved.
 //
 #import "utilities.h"
+#import <objc/runtime.h>
 
 void observeClassPropertyChanges(Class class)
 {
-
+    unsigned int propertyCount = 0;
+    objc_property_t *properties = class_copyPropertyList(class, &propertyCount);
+    for (unsigned int i=0; i<propertyCount; i++) {
+        objc_property_t property = properties[i];
+        // code
+    }
+    free(properties);
 }
