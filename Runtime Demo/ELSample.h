e1261d7ec50ae2e77fe2c8ba63a408408a23884a
Alexey Storozhev <dark.storoj@gmail.com>
2014-02-25 19:05:23 +0400

    sample class added

---
+++ Runtime Demo/ELSample.h
@@ -0,0 +1,11 @@
+//
+// Created by Alexey Storozhev on 25.02.14.
+// Copyright (c) 2014 e-Legion. All rights reserved.
+//
+
+@interface ELSample : NSObject
+
+@property (nonatomic, strong) NSArray *values;
+@property (nonatomic, assign, getter=isValid, setter=setValidCustom:) BOOL valid;
+
+@end
---
+++ Runtime Demo/ELSample.m
@@ -0,0 +1,11 @@
+//
+// Created by Alexey Storozhev on 25.02.14.
+// Copyright (c) 2014 e-Legion. All rights reserved.
+//
+#import "ELSample.h"
+
+@interface ELSample ()
+@end
+
+@implementation ELSample
+@end
